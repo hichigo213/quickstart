fb2f2ca9f2b73a8895982e0386b9c2b5c7e4ea03
hichigo213 <42939715+hichigo213@users.noreply.github.com>
2018-09-08 00:47:57 +0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use Illuminate\Support\Facades\Auth;
+
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    })->middleware('guest');
+
+    Route::get('/tasks', 'TaskController@index');
+    Route::post('/task', 'TaskController@store');
+    Route::delete('/task/{task}', 'TaskController@destroy');
+    Route::get('logout', 'Auth\LoginController@logout', function () {
+        return abort(404);
+    });
+});
+
+Auth::routes();
+
